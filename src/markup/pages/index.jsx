da21dbe6d66037d54d439aa7b56e81fe5ac45ed1
de-salman <salmanfarisi.bca@gmail.com>
2023-09-22 15:01:40 +0530

    22-09-23

--- src/markup/pages/index.jsx
+++ src/markup/pages/index.jsx
@@ -1,4 +1,4 @@
-import React  from 'react';
+import React, { useEffect }  from 'react';
 
 // Layout
 //import Header from "../layout/header";
@@ -16,6 +16,7 @@ import { useTranslation } from 'react-i18next';
 import HeroBanner from '../elements/widget/HeroBanner';
 import AboutUsFourSection from "../elements/about-us-4";
 import AboutUsThreeSection from "../elements/about-us-3";
+import { ElfsightWidget } from 'react-elfsight-widget';
 
 function Index() {
 
@@ -26,7 +27,7 @@ function Index() {
 	const handleRefresh = (url) => {
 		window.location.href = url;
 	};
-	
+
 		
 		return (
 			<>
@@ -56,7 +57,12 @@ function Index() {
 				<TeamOneSection />
 				
 				<TestimonialSection />
+				{/* <iframe src='https://widget-153800baf3784e00bcabad3c5675eb3c.elfsig.ht' width='100%' height='1000' frameborder='0'></iframe> */}
+				<div className='wid-cst'>
+				<ElfsightWidget widgetId={"153800ba-f378-4e00-bcab-ad3c5675eb3c"} lazy />;
+				<ElfsightWidget widgetId={"4d5c4a86-db7a-46b3-9d85-a65483b969c1"} lazy />;
 				
+				</div>
 				<LatestBlogSection />
 				
 				<Footer />
